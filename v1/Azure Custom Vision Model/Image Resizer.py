2b752bf45c13a949072a17c18e259a72170b1063
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2020-08-01 15:20:16 +0530

    Delete Image Resizer.py

--- v1/Azure Custom Vision Model/Image Resizer.py
+++
@@ -1,9 +0,0 @@
-import PIL 
-from PIL import Image
-import matplotlib.pyplot as plt
-import os
-basewidth = 244
-# img = Image.open("C:/Users/Dell/Downloads/COW_data/2Q__(1).jpg")
-path = os.path("C:/Users/Dell/Downloads/COW_data")
-for _ in path:
-    print(_)
