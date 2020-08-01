0a1e54a012c2014abc54cead033b7bab1bbc0d66
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2020-08-01 15:19:02 +0530

    Supporting Files Azure Model

---
+++ v1/Azure Custom Vision Model/Image Resizer.py
@@ -0,0 +1,9 @@
+import PIL 
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+basewidth = 244
+# img = Image.open("C:/Users/Dell/Downloads/COW_data/2Q__(1).jpg")
+path = os.path("C:/Users/Dell/Downloads/COW_data")
+for _ in path:
+    print(_)
